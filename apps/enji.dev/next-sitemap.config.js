7f20e85eef8eb5c166fe577a80c9c7969a642ec1
Dragan Nešković <neskovicdragan@hotmail.com>
2023-07-12 00:47:41 +0200

    built

--- apps/enji.dev/next-sitemap.config.js
+++ apps/enji.dev/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.enji.dev',
+  siteUrl: 'https://alchweb-enji-dev.vercel.app',
   priority: 0.6,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
