19ee66e352cd2751a2262ace7d484a592a160877
Dragan Nešković <neskovicdragan@hotmail.com>
2023-07-12 01:08:16 +0200

    logo fixed

--- apps/enji.dev/src/components/Logo.tsx
+++ apps/enji.dev/src/components/Logo.tsx
@@ -8,19 +8,17 @@ interface LogoProps {
 function Logo({ active = false }: LogoProps) {
   return (
     <div className={clsx('flex items-center gap-1.5 font-[1000] leading-none')}>
-      <Image
-        alt="logo"
-        src="/assets/images/logac.png"
-        width={32}
-        height={32}
-        className={clsx(
-          'hidden max-w-none',
-          'lg:block',
-          'dark:brightness-[.82]'
-        )}
-        quality={100}
-        priority
-      />
+      <div className={clsx('lg:block', 'dark:brightness-[.82]')}>
+        <Image
+          alt="logo"
+          src="/assets/images/logac.png"
+          width={32}
+          height={32}
+          className="max-w-none"
+          quality={100}
+          priority
+        />
+      </div>
       <div className={clsx('-mt-1 hidden text-xl', 'sm:block')}>
         <span className={clsx('text-slate-900', 'dark:text-slate-200')}>
           alchemy
