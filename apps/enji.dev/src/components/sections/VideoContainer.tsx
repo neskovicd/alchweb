afc4eed7eb0df8cdcaf8e44938aa1da9c1497e92
Dragan Nešković <neskovicdragan@hotmail.com>
2023-07-17 18:52:31 +0200

    autoplay mobile inc

--- apps/enji.dev/src/components/sections/VideoContainer.tsx
+++ apps/enji.dev/src/components/sections/VideoContainer.tsx
@@ -14,7 +14,14 @@ function VideoContainer({ videoSrc }: VideoContainerProps) {
           'border-accent-400 dark:border-accent-400 w-128 mt-16 mr-4 ml-4 flex justify-center rounded-2xl border-2 dark:bg-slate-900'
         )}
       >
-        <video width={500} height={300} className="rounded-2xl" autoPlay muted>
+        <video
+          width={500}
+          height={300}
+          className="rounded-2xl"
+          autoPlay
+          muted
+          playsInline
+        >
           <source src={videoSrc} type="video/webm" />
           {/* Add additional <source> tags for other video formats if necessary */}
           Your browser does not support the video tag.
